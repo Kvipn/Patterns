0f35aae4fff8a337d83b73a3466d8633a8ed055c
Mikita Kazlouski <kvipntv@gmail.com>
2022-02-05 14:32:48 +0300

    Abstract Factory code file for Units, feature fight between Enemys and Units

---
+++ AbstractFactory.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Patterns
+{
+    class UnitGenerator
+    {
+        public List<Unit> GetUnits(int unitAmount)
+        {
+            List<Unit> units = new List<Unit>();
+            Random random = new Random();
+            UnitFactory[] unitFactory = { new ArcherFactory(), new InfantryFactory(), new KnightFactory() };
+            for (int i = 0; i < unitAmount; i++)
+            {
+                units.Add(new Unit(unitFactory[random.Next(3)]));
+            }
+            return units;
+        
+        }
+    }
+
+    class Unit
+    {
+        Weapon firstWeapon;
+        Weapon secondWeapon;
+        Armor armor;
+        public int HitPoints { get; set; }
+        public int Damage { get; set; }
+        public string BattleRoar { get; set; }
+
+        public Unit(UnitFactory factory)
+        {
+            firstWeapon = factory.CreateFirstWeapon();
+            secondWeapon = factory.CreateSecondWeapon();
+            armor = factory.CreateArmor();
+            Damage = 10 + firstWeapon.Damage + secondWeapon.Damage;
+            HitPoints = 50 + armor.HitPoints;
+            switch (armor)
+            {
+                case LightArmor:BattleRoar = "Моя меткость паразит вас!"; break;
+                case MediumArmor:BattleRoar = "Пехтура никого не пропустит!"; break;
+                case HeavyArmor:BattleRoar = "Почуствуй кровь мой меч!"; break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    //class Archer : Unit
+    //{
+
+    //}
+    //class Infantry : Unit
+    //{
+
+    //}
+    //class Knight : Unit
+    //{
+
+    //}
+
+    abstract class Weapon
+    {
+        public int Damage { get; set; }
+        public string WeaponType { get; set; }
+    }
+
+    class Bow : Weapon
+    {
+        public Bow()
+        {
+            Damage = 20;
+            WeaponType = "Bow";
+        }
+    }
+    class Pike : Weapon
+    {
+        public Pike()
+        {
+            Damage = 30;
+            WeaponType = "Pike";
+        }
+    }
+    class Axe : Weapon
+    {
+        public Axe()
+        {
+            Damage = 15;
+            WeaponType = "Axe";
+        }
+    }
+    class Swodr : Weapon
+    {
+        public Swodr()
+        {
+            Damage = 50;
+            WeaponType = "Sword";
+        }
+    }
+
+    abstract class Armor
+    {
+        public int HitPoints { get; set; }
+        public string ArmorType { get; set; }
+    }
+
+    class LightArmor : Armor
+    {
+        public LightArmor()
+        {
+            HitPoints = 20;
+            ArmorType = "Light Armor";
+        }
+    }
+    class MediumArmor : Armor
+    {
+        public MediumArmor()
+        {
+            HitPoints = 40;
+            ArmorType = "Medium Armor";
+        }
+    }
+    class HeavyArmor : Armor
+    {
+        public HeavyArmor()
+        {
+            HitPoints = 60;
+            ArmorType = "Heavy Armor";
+        }
+    }
+
+    abstract class UnitFactory
+    {
+        public abstract Weapon CreateFirstWeapon();
+        public abstract Weapon CreateSecondWeapon();
+        public abstract Armor CreateArmor();
+    }
+
+    class ArcherFactory:UnitFactory
+    {
+        public override Weapon CreateSecondWeapon()
+        {
+            return new Axe();
+        }
+        public override Weapon CreateFirstWeapon()
+        {
+            return new Bow();
+        }
+        public override Armor CreateArmor()
+        {
+            return new LightArmor();
+        }
+    }
+    class InfantryFactory : UnitFactory
+    {
+        public override Weapon CreateSecondWeapon()
+        {
+            return new Axe();
+        }
+        public override Weapon CreateFirstWeapon()
+        {
+            return new Pike();
+        }
+        public override Armor CreateArmor()
+        {
+            return new MediumArmor();
+        }
+    }
+    class KnightFactory  : UnitFactory
+    {
+        public override Weapon CreateSecondWeapon()
+        {
+            return new Swodr();
+        }
+        public override Weapon CreateFirstWeapon()
+        {
+            return new Pike();
+        }
+        public override Armor CreateArmor()
+        {
+            return new LightArmor();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Patterns
 {
@@ -9,12 +10,52 @@ namespace Patterns
         {
             List<Enemy> enemies = new List<Enemy>();
             EnemyGenerator generator = new EnemyGenerator();
-            enemies.AddRange(generator.GetEnemies(12));
+            enemies.AddRange(generator.GetEnemies(18));
             
-            foreach (var item in enemies)
+            List<Unit> units = new List<Unit>();
+            UnitGenerator unitGenerator = new UnitGenerator();
+            units.AddRange(unitGenerator.GetUnits(12));
+
+            Console.WriteLine( Program.Fight(units,enemies));
+
+
+        }
+
+        static public string Fight(List<Unit> units , List<Enemy> enemies)
+        {
+            Random random = new Random();
+            int EnemyDamage = enemies.Sum(n=>n.Damage);
+            int EnemyHitpoints = enemies.Sum(n=>n.HitPoints);
+            int UnitDamage = units.Sum(n=>n.Damage);
+            int UnitHitpoints = units.Sum(n=>n.HitPoints);
+            
+            Console.WriteLine($"Суммарное здоровье людей {UnitHitpoints}.\r\n" +
+                    $"Суммарное здоровье нелюдей {EnemyHitpoints}");
+
+            for (int i = 0; ;i++)
             {
-                Console.WriteLine(item.BattleRoar);
+                Console.WriteLine($"Раунд {i+1}!");
+                EnemyHitpoints -= UnitDamage;
+                UnitHitpoints -= EnemyDamage;
+                if (EnemyHitpoints<=0 && UnitHitpoints<=0)
+                {
+                    return "Ничья!!!";
+                }
+                else if(EnemyHitpoints <= 0)
+                {
+                    return "Победа людей!";
+                }
+                else if(UnitHitpoints <= 0)
+                {
+                    return "Мир людей падет!";
+                }
+                Console.WriteLine($"Суммарное здоровье людей {UnitHitpoints}.\r\n" +
+                    $"Суммарное здоровье нелюдей {EnemyHitpoints}");
+                Console.WriteLine(units[random.Next(units.Count)].BattleRoar);
+                Console.WriteLine(enemies[random.Next(enemies.Count)].BattleRoar);
+                        
             }
+
         }
     }
 }
